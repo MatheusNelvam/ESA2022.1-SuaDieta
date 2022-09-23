7b54289e67278f11d090dba0d0ef88b48743d9b8
Kawhan Laurindo de Lima <kawhan.laurindo@labs.dcx>
2022-09-23 14:35:08 -0300

    commit_files
    Signed-off-by: Kawhan Laurindo de Lima <kawhan.laurindo@labs.dcx>

--- suaDieta/suaDieta/settings.py
+++ suaDieta/suaDieta/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -145,3 +146,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+PROJECT_ROOT = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(PROJECT_ROOT, '../apps'))
